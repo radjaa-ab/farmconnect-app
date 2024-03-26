2c37c0e3a5c3576188d4662a589dfdc4c9f1aee3
Thanaa-messaouden <thanaamessaouden@gmail.com>
2024-03-26 14:46:22 +0100

    fixed npm start errors

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 // App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Switch>
+        <Routes>
           <Route exact path="/" component={Home} />
           {/* Add more routes here */}
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
