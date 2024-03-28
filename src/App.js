062f3468e1ea18e2d01ad81af00c63c79c25951e
thana <thanaamessaoudene01@gmail.com>
2024-03-28 10:20:24 +0100

    Removed all functions and databse implementation, mis-connected

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
-import Maain from "./Pages/Maain";
-import Login from "./Pages/Login";
-import Register from "./Pages/Register";
+// App.js
+
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
-import Messaging from './Pages/Messaging'
-import './Styles/Style.css';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useContext } from "react";
-import { AuthContext } from "./context/AuthContext";
+import Messaging from './Pages/Messaging';
 
 function App() {
-  const { currentUser } = useContext(AuthContext);
-
-  const ProtectedRoute = ({ children }) => {
-    if (!currentUser) {
-      return <Navigate to="/login" />;
-    }
-
-    return children
-  };
-
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <ProtectedRoute>
-                <Maain />
-              </ProtectedRoute>
-            }
-          />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="Home/" element={<Home />} />
-          <Route path="Messaging/" element={<Messaging />} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Messaging/" element={<Messaging />}/>
+        </Routes>
     </BrowserRouter>
   );
 }
