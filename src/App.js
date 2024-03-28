0cc9af72981fc2c774dc36266a7856f3311b04dd
thana <thanaamessaoudene01@gmail.com>
2024-03-28 10:33:11 +0100

    Fixed Routes

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import Maain from "./Pages/Maain";
 import Home from './Pages/Home';
 import Messaging from './Pages/Messaging'
 import './Styles/Style.css';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/">
-          <Route
-            index
-            element={
-              <Route>
-                <Maain />
-              </Route>
-            }
-          />
+        <Route path="/" element={<Maain />} />
           <Route path="Home/" element={<Home />} />
           <Route path="Messaging/" element={<Messaging />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
