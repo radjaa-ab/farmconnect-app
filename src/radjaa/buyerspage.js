5f1ffa772c150ca8155f04d181060e091559215c
radjaa-ab <radjaaayaabbouu@gmail.com>
2024-03-29 17:49:52 +0100

    Changes to be committed: 	new file:   src/radjaa/buyerspage.js 	new file:   src/radjaa/homepage.js

---
+++ src/radjaa/buyerspage.js
@@ -0,0 +1,300 @@
+import React from 'react';
+import './index.css';
+
+export default function Main() {
+  return (
+    <div className='main-container'>
+      <div className='rectangle'>
+        <div className='call-now'>
+          <div className='phone-call'>
+            <div className='group' />
+          </div>
+          <span className='phone-number'>9430144469</span>
+        </div>
+        <div className='left-contents'>
+          <div className='social'>
+            <div className='facebook-icon'>
+              <div className='vector' />
+            </div>
+            <div className='twitter-icon'>
+              <div className='vector-1' />
+            </div>
+            <div className='pinterest-icon' />
+            <div className='linkedin-icon'>
+              <div className='vector-2' />
+            </div>
+          </div>
+          <span className='welcome-message'>Bienvenue à FarmConnect </span>
+        </div>
+      </div>
+      <div className='flex-row-eb'>
+        <div className='search'>
+          <div className='search-3'>
+            <div className='group-4' />
+          </div>
+          <span className='search-text'>Search</span>
+        </div>
+        <div className='nav-links'>
+          <div className='nav-links-5'>
+            <div className='nav-links-6'>
+              <span className='home-link'>Accueil</span>
+              <div className='chevron-down' />
+            </div>
+            <div className='nav-links-7'>
+              <span className='products-link'>Produits</span>
+              <div className='chevron-down-8' />
+            </div>
+            <div className='nav-links-9'>
+              <span className='equipments-link'>Equipements</span>
+              <div className='chevron-down-a' />
+            </div>
+            <div className='nav-links-b'>
+              <span className='blog-link'>Blog</span>
+              <div className='chevron-down-c' />
+            </div>
+            <div className='nav-links-d'>
+              <span className='about-link'>A propos</span>
+            </div>
+            <div className='nav-links-e'>
+              <span className='contact-link'>Contactez-nous</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='banner'>
+        <div className='big-banner' />
+      </div>
+      <div className='heading'>
+        <span className='popular-categories'>Categories</span>
+        <div className='button'>
+          <span className='view-all'>Voir tout</span>
+          <div className='group-f' />
+        </div>
+      </div>
+      <div className='flex-row-a'>
+        <div className='category'>
+          <div className='image' />
+          <span className='fresh-fruit'>Fruit frais</span>
+        </div>
+        <div className='category-10'>
+          <div className='image-11' />
+          <span className='fresh-vegetables'>Légumes frais</span>
+        </div>
+        <div className='category-12'>
+          <div className='image-13' />
+          <span className='equipment'>Equipements</span>
+        </div>
+        <div className='category-14'>
+          <div className='image-15' />
+          <span className='land'>Terrains</span>
+        </div>
+      </div>
+      <div className='heading-16'>
+        <span className='fruits-vegetables'>Légumes et Fruits</span>
+        <div className='button-17'>
+          <span className='view-all-18'>Voir tout</span>
+          <div className='group-19' />
+        </div>
+      </div>
+      <div className='flex-row-ed'>
+        <div className='product'>
+          <div className='product-image'>
+            <div className='image-1a' />
+          </div>
+          <div className='product-info'>
+            <div className='info'>
+              <span className='green-apple'>Pomme vert</span>
+              <div className='price'>
+                <span className='dollar'>100</span>
+              </div>
+            </div>
+          </div>
+          <div className='container' />
+          <div className='rectangle-1b' />
+        </div>
+        <div className='product-n'>
+          <div className='product-image-1c'>
+            <div className='image-1d' />
+          </div>
+          <div className='product-info-1e'>
+            <div className='info-1f'>
+              <span className='green-apple-20'>Orange</span>
+              <div className='price-21'>
+                <span className='dollar-22'>120.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-23' />
+          <div className='rectangle-24' />
+        </div>
+        <div className='product-n-25'>
+          <div className='product-image-26'>
+            <div className='image-27' />
+          </div>
+          <div className='product-info-28'>
+            <div className='info-29'>
+              <span className='green-apple-2a'>Chou</span>
+              <div className='price-2b'>
+                <span className='dollar-2c'>112.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-2d' />
+          <div className='rectangle-2e' />
+        </div>
+        <div className='product-n-2f'>
+          <div className='product-image-30'>
+            <div className='image-31' />
+          </div>
+          <div className='product-info-32'>
+            <div className='info-33'>
+              <span className='green-apple-34'>Laitue verte</span>
+              <div className='price-35'>
+                <span className='dollar-36'>9.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-37' />
+          <div className='rectangle-38' />
+        </div>
+        <div className='product-n-39'>
+          <div className='product-image-3a'>
+            <div className='image-3b' />
+          </div>
+          <div className='product-info-3c'>
+            <div className='info-3d'>
+              <span className='green-apple-3e'>Aubergine</span>
+              <div className='price-3f'>
+                <span className='dollar-40'>34.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-41' />
+          <div className='rectangle-42' />
+        </div>
+      </div>
+      <div className='flex-row-fc'>
+        <div className='product-n-43'>
+          <div className='product-image-44'>
+            <div className='image-45' />
+          </div>
+          <div className='product-info-46'>
+            <div className='info-47'>
+              <span className='green-apple-48'>Big Potatoes</span>
+              <div className='price-49'>
+                <span className='dollar-4a'>20.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-4b' />
+          <div className='rectangle-4c' />
+        </div>
+        <div className='product-n-4d'>
+          <div className='product-image-4e'>
+            <div className='image-4f' />
+          </div>
+          <div className='product-info-50'>
+            <div className='info-51'>
+              <span className='green-apple-52'>Maïs</span>
+              <div className='price-53'>
+                <span className='dollar-54'>20.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-55' />
+          <div className='rectangle-56' />
+        </div>
+        <div className='product-name'>
+          <div className='product-image-57'>
+            <div className='image-58' />
+          </div>
+          <div className='product-info-59'>
+            <div className='info-5a'>
+              <span className='green-apple-5b'>Chou-fleur frais</span>
+              <div className='price-5c'>
+                <span className='price-value'>12.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-5d' />
+          <div className='rectangle-5e' />
+        </div>
+        <div className='product-name-5f'>
+          <div className='product-image-60'>
+            <div className='image-61' />
+          </div>
+          <div className='product-info-62'>
+            <div className='info-63'>
+              <span className='green-apple-64'>Poivron vert</span>
+              <div className='price-65'>
+                <span className='price-value-66'>9.00</span>
+                <span className='price-symbol' />
+              </div>
+            </div>
+          </div>
+          <div className='container-67' />
+          <div className='rectangle-68' />
+        </div>
+        <div className='product-name-69'>
+          <div className='product-image-6a'>
+            <div className='image-6b' />
+          </div>
+          <div className='product-info-6c'>
+            <div className='info-6d'>
+              <span className='green-apple-6e'>Chili vert</span>
+              <div className='price-6f'>
+                <span className='price-value-70'>34.00</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-71' />
+          <div className='rectangle-72' />
+        </div>
+      </div>
+      <div className='heading-73'>
+        <span className='popular-categories-74'>Equipements</span>
+        <div className='button-75'>
+          <span className='view-all-76'>Voir tout</span>
+          <div className='group-77' />
+        </div>
+      </div>
+      <div className='flex-row-fdce'/>
+        <div className='product-name-78'>
+          <div className='product-image-79'>
+            <div className='image-7a' />
+          </div>
+          <div className='product-info-7b'>
+            <div className='info-7c'>
+              <span className='green-apple-7d'>Green Apple</span>
+              <div className='price-7e'>
+                <span className='price-value-7f'>200</span>
+                <span className='price-symbol-80' />
+              </div>
+            </div>
+          </div>
+          <div className='container-81' />
+          <div className='rectangle-82' />
+        </div>
+        <div className='product-name-83'>
+          <div className='product-image-84'>
+            <div className='image-85' />
+          </div>
+          <div className='product-info-86'>
+            <div className='info-87'>
+              <span className='green-apple-88'>Fresh Indian Malta</span>
+              <div className='price-89'>
+                <span className='price-value-8a'>300</span>
+              </div>
+            </div>
+          </div>
+          <div className='container-8b' />
+          <div className='rectangle-8c' />
+        </div>
+        <div className='product-name-8d'/>
+          <div className='product-image-8e'>
+            <div className='image-8f' />
+          </div>
+          <div className='product-info-90'/>
+            <div className='info-91'/>
+              <span className='green-apple-92'>Indian cabbage</span>
+              <div className='price-93'></div>
