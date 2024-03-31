faf657a5053afc4e7897a27ad2ee9778a5d5051f
Thanaa-messaouden <thanaamessaouden@gmail.com>
2024-03-31 10:43:25 +0100

    I FORGOT

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,36 +1,18 @@
-import React, { useContext, useEffect, useRef } from "react";
-import { AuthContext } from "../context/AuthContext";
-import { ChatContext } from "../context/ChatContext";
+import React from "react";
 
-const Message = ({ message }) => {
-  const { currentUser } = useContext(AuthContext);
-  const { data } = useContext(ChatContext);
-
-  const ref = useRef();
-
-  useEffect(() => {
-    ref.current?.scrollIntoView({ behavior: "smooth" });
-  }, [message]);
+const Message = () => {
 
   return (
-    <div
-      ref={ref}
-      className={`message ${message.senderId === currentUser.uid && "owner"}`}
-    >
+    <div>
       <div className="messageInfo">
         <img
-          src={
-            message.senderId === currentUser.uid
-              ? currentUser.photoURL
-              : data.user.photoURL
-          }
           alt=""
         />
         <span>just now</span>
       </div>
       <div className="messageContent">
-        <p>{message.text}</p>
-        {message.img && <img src={message.img} alt="" />}
+        <p>Message Content</p>
+        <img alt="" />
       </div>
     </div>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,17 @@
-import React, { useContext } from 'react'
-import {signOut} from "firebase/auth"
-import { auth } from '../firebase'
-import { AuthContext } from '../context/AuthContext'
+import React from 'react'
 
 const Navbar = () => {
-  const {currentUser} = useContext(AuthContext)
 
   return (
     <div className='navbar'>
-      <span className="logo">FarmConnect</span>
+      <span className="logo">Lama Chat</span>
       <div className="user">
-        <img src={currentUser.photoURL} alt="" />
-        <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>logout</button>
+        <img alt="" />
+        <span>Name</span>
+        <button>logout</button>
       </div>
     </div>
   )
 }
 
-export default Navbar
+export default Navbar;
