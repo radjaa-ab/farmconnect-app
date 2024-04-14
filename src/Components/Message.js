7f2cb43a27e005ed0296c454c48e60d9f8f16e44
itsafaf <mynameafaf20@gmail.com>
2024-04-15 00:03:23 +0100

    commit

--- src/Components/Message.js
+++ src/Components/Message.js
@@ -1,17 +1,24 @@
 import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { ChatContext } from "../context/ChatContext";
+import { useTranslation } from 'react-i18next';
+import i18n from '../i18n';
 
 const Message = ({ message }) => {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  };
   const { currentUser } = useContext(AuthContext);
   const { data } = useContext(ChatContext);
 
-  console.log("ID de l'utilisateur actuel :", currentUser.uid);
-  console.log("ID de l'expéditeur du message :", message.senderId);
+  console.log(t("ID de l'utilisateur actuel :"), currentUser.uid);
+  console.log(t("Message Sender ID:"), message.senderId);
   if (message.senderId === currentUser.uid) {
-    console.log("Ce message appartient à l'utilisateur actuel.");
+    console.log(t("This message belongs to the current user."));
   } else {
-    console.log("Ce message n'appartient pas à l'utilisateur actuel.");
+    console.log(t("This message does not belong to the current user."));
   }
 
   const ref = useRef();
--- src/Components/Panier.js
+++ src/Components/Panier.js
@@ -11,8 +11,15 @@ import {
     MDBTypography,
     } from "mdb-react-ui-kit";
     import React from "react";
+    import { useTranslation } from 'react-i18next';
+    import i18n from '../i18n';
     
     export default function Panier() {
+      const { t, i18n } = useTranslation();
+
+      const changeLanguage = lng => {
+    i18n.changeLanguage(lng);
+  }; 
     return (
     <section className="h-100 h-custom" style={{ backgroundColor: "#eee" }}>
       <MDBContainer className="py-5 h-100">
@@ -24,8 +31,7 @@ import {
                   <MDBCol lg="7">
                     <MDBTypography tag="h5">
                       <a href="#!" className="text-body">
-                        <MDBIcon fas icon="long-arrow-alt-left me-2" /> Continue
-                        shopping
+                        <MDBIcon fas icon="long-arrow-alt-left me-2" /> {t("Continue shopping")}
                       </a>
                     </MDBTypography>
     
@@ -33,14 +39,14 @@ import {
     
                     <div className="d-flex justify-content-between align-items-center mb-4">
                       <div>
-                        <p className="mb-1">Shopping cart</p>
-                        <p className="mb-0">You have 4 items in your cart</p>
+                        <p className="mb-1">{t("Shopping cart")}</p>
+                        <p className="mb-0">{t("You have 4 items in your cart")}</p>
                       </div>
                       <div>
                         <p>
-                          <span className="text-muted">Sort by:</span>
+                          <span className="text-muted">{t("Sort by:")}</span>
                           <a href="#!" className="text-body">
-                            price
+                           {t(" price")}
                             <MDBIcon fas icon="angle-down mt-1" />
                           </a>
                         </p>
@@ -55,13 +61,13 @@ import {
                               <MDBCardImage
                                 src=" "
                                 fluid className="rounded-3" style={{ width: "65px" }}
-                                alt="Shopping item" />
+                                alt={4("Shopping item")} />
                             </div>
                             <div className="ms-3">
                               <MDBTypography tag="h5">
-                                Product
+                                {t("Product")}
                               </MDBTypography>
-                              <p className="small mb-0">Description</p>
+                              <p className="small mb-0">{t("Description")}</p>
                             </div>
                           </div>
                           <div className="d-flex flex-row align-items-center">
@@ -72,7 +78,7 @@ import {
                             </div>
                             <div style={{ width: "80px" }}>
                               <MDBTypography tag="h5" className="mb-0">
-                                Price
+                                {t("Price")}
                               </MDBTypography>
                             </div>
                             <a href="#!" style={{ color: "#cecece" }}>
