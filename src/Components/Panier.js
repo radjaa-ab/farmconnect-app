20743c09ab2990d0cde569b37fd3b07b0a637b1d
radjaa-ab <radjaaayaabbouu@gmail.com>
2024-04-21 21:15:37 +0200

    chwiya khnatech f panier

--- src/Components/Panier.js
+++ src/Components/Panier.js
@@ -25,80 +25,101 @@ export default function Panier() {
       <Navigation />
 
       {/* Content */}
-      <section className="h-100 h-custom" style={{ backgroundColor: "#2ecc71" }}>
-        <MDBContainer className="py-5 h-100">
-          <MDBRow className="justify-content-center align-items-center h-100">
-            <MDBCol>
-              <MDBCard className="border-0">
-                <MDBCardBody className="p-4">
-                  <MDBRow>
-                    <MDBCol lg="7">
-                      <MDBTypography tag="h5">
-                        <a href="#!" className="text-body text-decoration-none">
-                          <MDBIcon fas icon="long-arrow-alt-left me-2" /> {t("Continue shopping")}
-                        </a>
-                      </MDBTypography>
+      <MDBContainer className="py-5">
+        <MDBRow className="justify-content-center align-items-center h-100">
+          <MDBCol lg="7">
+            <MDBCard className="border-0 shadow rounded-3" style={{ backgroundColor: "#fff" }}>
+              <MDBCardBody className="p-4">
+                <MDBRow>
+                  <MDBCol>
+                    <MDBTypography tag="h5">
+                      <a href="#!" className="text-body text-decoration-none">
+                        <MDBIcon fas icon="long-arrow-alt-left me-2" /> {t("Continue shopping")}
+                      </a>
+                   </MDBTypography>
 
-                      <hr className="my-4" style={{ borderColor: "#fff" }} />
+                    <hr className="my-4" style={{ borderColor: "#fff" }} />
 
-                      <div className="d-flex justify-content-between align-items-center mb-4">
-                        <div>
-                          <p className="mb-1 text-white">{t("Shopping cart")}</p>
-                          <p className="mb-0 text-white">{t("You have 4 items in your cart")}</p>
-                        </div>
-                        <div>
-                          <p>
-                            <span className="text-muted">{t("Sort by:")}</span>
-                            <a href="#!" className="text-white">
-                              {t("price")}
-                              <MDBIcon fas icon="angle-down mt-1" />
-                            </a>
-                          </p>
-                        </div>
+                    <div className="d-flex justify-content-between align-items-center mb-4">
+                      <div>
+                        <p className="mb-1 text-white">{t("Shopping cart")}</p>
+                        <p className="mb-0 text-white">{t("You have 4 items in your cart")}</p>
                       </div>
+                      <div>
+                        <p>
+                          <span className="text-muted">{t("Sort by:")}</span>
+                          <a href="#!" className="text-white">
+                            {t("price")}
+                            <MDBIcon fas icon="angle-down" className="ms-2" />
+                          </a>
+                        </p>
+                      </div>
+                    </div>
 
-                      <MDBCard className="mb-3 border-0" style={{ backgroundColor: "#fff" }}>
-                        <MDBCardBody className="d-flex justify-content-between align-items-center">
-                          <div className="d-flex flex-row align-items-center">
-                            <div>
-                              <MDBCardImage
-                                src=" "
-                                fluid
-                                className="rounded-3"
-                                style={{ width: "65px" }}
-                                alt={t("Shopping item")}
-                              />
-                            </div>
-                            <div className="ms-3">
-                              <MDBTypography tag="h5">{t("Product")}</MDBTypography>
-                              <p className="small mb-0">{t("Description")}</p>
-                            </div>
+                    {/* Cart items */}
+                    <MDBRow>
+                      <MDBCol lg="5">
+                        <MDBCardImage className="rounded" src="https://via.placeholder.com/150" alt="Product 1" />
+                      </MDBCol>
+                      <MDBCol lg="7">
+                        <div className="d-flex justify-content-between align-items-center">
+                          <div>
+                            <h5 className="mb-0">{t("Product 1")}</h5>
+                            <p className="mb-0 text-muted">{t("$25")}</p>
                           </div>
-                          <div className="d-flex flex-row align-items-center">
-                            <div style={{ width: "50px" }}>
-                              <MDBTypography tag="h5" className="fw-normal mb-0">
-                                2
-                              </MDBTypography>
-                            </div>
-                            <div style={{ width: "80px" }}>
-                              <MDBTypography tag="h5" className="mb-0">
-                                {t("price")}
-                              </MDBTypography>
-                            </div>
-                            <a href="#!" style={{ color: "#cecece" }}>
-                              <MDBIcon fas icon="trash-alt" />
-                            </a>
+                          <div>
+                            <MDBInput
+                              type="number"
+                              value="1"
+                              min="1"
+                              className="mb-0"
+                              label={t("Quantity")}
+                              disabled
+                            />
                           </div>
-                        </MDBCardBody>
-                      </MDBCard>
-                    </MDBCol>
-                  </MDBRow>
-                </MDBCardBody>
-              </MDBCard>
-            </MDBCol>
-          </MDBRow>
-        </MDBContainer>
-      </section>
+                        </div>
+                      </MDBCol>
+                    </MDBRow>
+
+                    {/* Add more products */}
+                    <MDBRow className="mt-4">
+                      <MDBCol lg="6">
+                        <MDBTypography tag="h5">
+                          <a href="#!" className="text-body text-decoration-none">
+                            <MDBIcon fas icon="long-arrow-alt-left me-2" /> {t("Add more products")}
+                          </a>
+                        </MDBTypography>
+                      </MDBCol>
+                      <MDBCol lg="6">
+                        <MDBBtn className="mb-4" color="danger" size="md" waves="light" hover wavesEffect="on-hover">
+                          {t("Proceed to checkout")}
+                        </MDBBtn>
+                      </MDBCol>
+                    </MDBRow>
+                  </MDBCol>
+                </MDBRow>
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+
+      {/* Subtotal */}
+      <MDBContainer className="py-5">
+        <MDBRow>
+          <MDBCol md="6">
+            <MDBTypography tag="h5" className="fw-bold mb-3" style={{ color: "#2ecc71" }}>
+              {t("Subtotal")}
+            </MDBTypography>
+            <MDBTypography tag="h5" className="fw-normal mb-0">
+              {t("price")}
+            </MDBTypography>
+          </MDBCol>
+          <MDBCol md="6">
+            
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
 
       {/* Footer */}
       <Footer />
