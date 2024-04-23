b12dcdc4b9f058bb1187f69fdd4d66c33a0723e0
radjaa-ab <radjaaayaabbouu@gmail.com>
2024-04-23 22:29:35 +0200

    nothing chkil f account

--- src/Pages/Account.js
+++ src/Pages/Account.js
@@ -3,8 +3,11 @@ import './SettingsPage.css'; // Import CSS file for styling
 import BasicExample from '../Components/Navigation'; // Import the navigation bar component
 import Footer from '../Components/footer';
 import img from '../Images/Mourad.png'; // Import the user's photo
+import { useTranslation } from 'react-i18next'; // Import the useTranslation hook
 
 const Account = () => {
+  const { t } = useTranslation(); // Use the useTranslation hook for translation
+
   return (
     <div>
       <BasicExample /> {/* Include the navigation bar */}
@@ -17,49 +20,28 @@ const Account = () => {
               className="profile-photo"
             />
             <div className="profile-info">
-              <div className="profile-name">Mourad</div>
-              <div className="profile-email">Mourad@email.com</div>
+              <div className="profile-name">{t('Mourad')}</div>
+              <div className="profile-email">{t('Mourad@email.com')}</div>
             </div>
           </div>
         </div>
         <div className="profile-main">
           <div className="profile-header">
-            <div className="profile-header__title">Mon Profile</div>
+            <div className="profile-header__title">{t('Mon Profil')}</div>
           </div>
           <div className="profile-content">
             <div className="profile-section">
-              <h2>A Propos de moi</h2>
-              <p>Je suis passionné par l'agriculture durable et je m'efforce de développer des solutions innovantes pour relever les défis auxquels est confronté le secteur agricole. Fort d'une expérience en ingénierie et d'une profonde compréhension des pratiques agricoles, je suis déterminé à optimiser les processus agricoles, à réduire l'impact environnemental et à garantir la sécurité alimentaire pour les générations futures.</p>
+              <h2>{t('À Propos de moi')}</h2>
+              <p>{t('Je suis passionné par l\'agriculture durable et je m\'efforce de développer des solutions innovantes pour relever les défis auxquels est confronté le secteur agricole. Fort d\'une expérience en ingénierie et d\'une profonde compréhension des pratiques agricoles, je suis déterminé à optimiser les processus agricoles, à réduire l\'impact environnemental et à garantir la sécurité alimentaire pour les générations futures.')}</p>
             </div>
             <div className="profile-section">
-              <h2>Interests</h2>
+              <h2>{t('Intérêts')}</h2>
               <ul>
-                <li>Conception : J'apprécie la création et la mise en œuvre de technologies et de solutions agricoles innovantes.</li>
-                <li>Agriculture Durable : Je m'engage à promouvoir des pratiques agricoles respectueuses de l'environnement.</li>
-                <li>Engagement Communautaire : Je crois en l'établissement de relations solides au sein de la communauté agricole et en le partage des connaissances et des ressources pour un bénéfice mutuel.</li>
-                <li>Activités en Plein Air : Dans mes temps libres, j'apprécie passer du temps en plein air, explorer la nature et m'adonner à des activités telles que la randonnée et le jardinage.</li>
-              
+                <li>{t('Conception : J\'apprécie la création et la mise en œuvre de technologies et de solutions agricoles innovantes.')}</li>
+                <li>{t('Agriculture Durable : Je m\'engage à promouvoir des pratiques agricoles respectueuses de l\'environnement.')}</li>
+                <li>{t('Engagement Communautaire : Je crois en l\'établissement de relations solides au sein de la communauté agricole et en le partage des connaissances et des ressources pour un bénéfice mutuel.')}</li>
+                <li>{t('Activités en Plein Air : Dans mes temps libres, j\'apprécie passer du temps en plein air, explorer la nature et m\'adonner à des activités telles que la randonnée et le jardinage.')}</li>
               </ul>
-            </div>
-            <div className="profile-section">
-              <h2>Photos</h2>
-              <div className="profile-photos">
-                <img
-                  className="profile-photo"
-                  src="https://via.placeholder.com/150"
-                  alt="Photo 1"
-                />
-                <img
-                  className="profile-photo"
-                  src="https://via.placeholder.com/150"
-                  alt="Photo 2"
-                />
-                <img
-                  className="profile-photo"
-                  src="https://via.placeholder.com/150"
-                  alt="Photo 3"
-                />
-              </div>
             </div>
           </div>
         </div>
