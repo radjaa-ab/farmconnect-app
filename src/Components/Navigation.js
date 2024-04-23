72f8ad026aca37698321c669644eeb0bc6fca173
itsafaf <mynameafaf20@gmail.com>
2024-04-23 21:14:50 +0100

    badelt navigation.js

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,44 +6,40 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from '../Images/logo.jpg';
 import Cart from '../Images/chariot-de-chariot.png';
 import Call from '../Images/silhouette-de-messages.png';
-import { useNavigate } from 'react-router-dom'; // Importez useNavigate depuis react-router-dom
+import { useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import SearchBar from './SearchBar';
 import LanguageSelector from './LanguageSelector';
 import '../Styles/Home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useTranslation } from 'react-i18next';
-import i18n from '../i18n';
 
 function Navigation() {
   const { t, i18n } = useTranslation();
-
-  const changeLanguage = lng => {
+  
+  const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
   };
 
   const ProductsPlaceholder = t("search for products");
 
-  const navigate = useNavigate(); // Initialisez useNavigate
-  
-  // Fonction de gestion du clic sur le lien des paramètres
-  const handleSettingsClick = () => {
-    navigate('/Account'); // Redirigez l'utilisateur vers la page des paramètres
+  const navigate = useNavigate();
+
+  const handlePClick = () => {
+    navigate('/Products');
   };
 
-  const handlePClick =  () => {
-    navigate('/Products') ;// Redirigez l
-  }
+  const handleEClick = () => {
+    navigate('/Equipements');
+  };
 
-  const handleEClick =  () => {
-    navigate('/Equipements') ;// Redirigez l
-  }
+  const handleAccountClick = () => {
+    navigate('/Account');
+  };
 
-  const handlePanClick =  () => {
-    navigate('/Panier') ;// Redirigez l
-  }
-  // Fonction de gestion du clic sur l'image d'appel
   const handleCallClick = () => {
-    navigate(""); // Appel vers le numéro 0657783698
+    window.location.href = 'tel:0657783698';
   };
 
   return (
@@ -56,15 +52,18 @@ function Navigation() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="d-flex justify-content-between">
-            <Nav.Link className="ms-3" style={{ marginRight: '1rem' }} onClick={handleSettingsClick}>{t("Myaccount")}</Nav.Link>
+            <Nav.Link className="ms-3" style={{ marginRight: '1rem' }} onClick={handlePClick}>{t("main d'oeuvre")}</Nav.Link>
             <Nav.Link className="ms-3" style={{ marginRight: '1rem' }} onClick={handlePClick}>{t("products")}</Nav.Link>
             <Nav.Link className="ms-3" style={{ marginRight: '1rem' }} onClick={handleEClick}>{t("equipements")}</Nav.Link>
             <SearchBar placeholderText={ProductsPlaceholder} />
             <div style={{ padding: '7px'}}>
               <LanguageSelector />
             </div>
-            <Nav.Link  onClick={handlePanClick}>
-              <img src={Cart} alt={t("cart")} style={{ width: '25px' }} />
+            <Nav.Link onClick={handleAccountClick}>
+              <FontAwesomeIcon icon={faUser} style={{ fontSize: '20px', marginRight: '10px' }} />
+            </Nav.Link>
+            <Nav.Link onClick={handlePClick}>
+              <img src={Cart} alt={t("cart")} style={{ width: '25px', marginRight: '10px' }} />
             </Nav.Link>
             <Nav.Link onClick={handleCallClick}>
               <img src={Call} alt="email us" style={{ width: '25px' }} />
