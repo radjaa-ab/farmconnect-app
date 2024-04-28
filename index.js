d28c01a34fcdf56d502a91027b9b729d9ac39a00
radjaa-ab <radjaaayaabbouu@gmail.com>
2024-04-28 15:23:29 +0200

    Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
 import "./global.css";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
